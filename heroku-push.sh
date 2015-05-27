a4a5bb563a6b4c1eeb66759b65fdb08270211ea8
Marcel Puyat <marcelp@alumni.stanford.edu>
2015-05-26 22:17:02 -0700

    Change session age to 2 months

---
+++ heroku-push.sh
@@ -0,0 +1,5 @@
+
+./addFonts.sh;
+echo "Add \"SuperTask\" to title tag and paste: ";
+echo "<link href='https://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'> into css";
+echo "Then do: grunt buildcontrol:heroku to finalize push"
