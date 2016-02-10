9851ff791266287e0ba8d5846bb0ce0b54cc6e58
Marcel Puyat <marcelp@alumni.stanford.edu>
2016-02-09 18:11:55 -0800

    A

--- heroku-push.sh
+++
@@ -1,5 +0,0 @@
-grunt build;
-./addFonts.sh;
-echo "Add \"SuperTask\" to title tag and paste: ";
-echo "<link href='https://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'> into css";
-echo "Then do: grunt buildcontrol:heroku to finalize push"
